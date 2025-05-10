5031181f72fb11a3fc8726fd277f91393250f6bd
Phuc000 <truongvinhphuc2003@gmail.com>
2025-05-10 18:51:01 +0700

    fix recipe card

--- src/Components/Common/RecipeCard/RecipeCard.jsx
+++ src/Components/Common/RecipeCard/RecipeCard.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import { 
+  Card, 
+  CardActionArea, 
+  CardContent, 
+  CardMedia, 
+  Typography, 
+  Box, 
+  Rating 
+} from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import './RecipeCard.scss';
 
 const RecipeCard = ({ recipe, onClick }) => {
   return (
     <div className="recipe-card" onClick={() => onClick(recipe)}>
       <img src={recipe.image} alt={recipe.title} />
-      <h2>{recipe.title}</h2>
+      <h2 className='recipe-tittle'>{recipe.title}</h2>
       <div className="recipe-info">
-        <span className="recipe-time">
-          <i className="fa fa-clock-o"></i> {recipe.readyInMinutes || '30'} mins
-        </span>
-        <span className="recipe-servings">
-          <i className="fa fa-users"></i> {recipe.servings || '4'} servings
-        </span>
+        <Box>
+            <Rating 
+              value={recipe.averageRating || 0} 
+              readOnly 
+              precision={0.5} 
+              size="small"
+            />
+          </Box>
+          
+          <Box 
+            display="flex" 
+            justifyContent="space-between" 
+          >
+            <Box display="flex" alignItems="center">
+              <AccessTimeIcon fontSize="small" color="action" sx={{ mr: 0.5 }} />
+              <Typography variant="body2" color="text.secondary">
+                {recipe.readyInMinutes || '30'} mins
+              </Typography>
+            </Box>
+            
+            <Box display="flex" alignItems="center">
+              <PeopleAltIcon fontSize="small" color="action" sx={{ mr: 0.5 }} />
+              <Typography variant="body2" color="text.secondary">
+                {recipe.servings || '4'}
+              </Typography>
+            </Box>
+          </Box>
       </div>
     </div>
   );
